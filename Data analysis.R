a7aad357a781dccc3d23fddd2482e24d70ac1614
audhalbritter <audhalbritter.com>
2018-07-04 08:13:24 +0200

    initial commit

---
+++ Data analysis.R
@@ -0,0 +1,82 @@
+#### DATA ANALYSIS ####
+source("Load Data from database.R")
+
+library("lme4")
+library("broom")
+
+
+# 1) Does proportion of fertility differ across years?
+
+# fit model
+fit <- fertile %>% ungroup() %>% mutate(year = factor(year)) %>% 
+  glm(cbind(SumOffertile, NumberOfOccurrence - SumOffertile) ~ year, data = ., family = "quasibinomial")
+summary(fit)
+anova(fit, test = "F")
+
+summary(multcomp::glht(fit, multcomp::mcp(year = "Tukey")))
+
+
+output <- augment(fit) %>% 
+  select(year, .fitted, .se.fit) %>% 
+  as.tibble() %>% 
+  mutate(response = plogis(.fitted)) %>% 
+  mutate(se2 = logit2prob(output$.se.fit)) %>% 
+  mutate(se = plogis(.se.fit))
+  
+
+# Backtransform logit
+logit2prob <- function(logit){
+  odds <- exp(logit)
+  prob <- odds / (1 + odds)
+  return(prob)
+}
+
+
+output %>% 
+  group_by(year) %>% 
+  summarise(response = mean(response), se = mean(se)) %>% 
+  ggplot(aes(y = response, x = year, ymin = response -se, ymax = response + se)) +
+  geom_point() +
+  geom_errorbar(width = 0) +
+  ylim(0.08, 0.17)
+  
+
+
+# GLMER solution
+fit <- glmer(cbind(SumOffertile, NumberOfOccurrence - SumOffertile) ~ factor(year) + (1|species) + (1|blockID), data = fertile, family = "binomial")
+summary(fit)
+
+
+
+
+
+
+
+
+# 2) Does variation in the proportion of fertility differ across the climate grid?
+  
+  
+  
+  
+
+
+
+# Is the temporal variation in proportion of flowering related to Mean summer Temp, mean temp or ann. prec or previous year climate?
+
+mod01 <- lmer(asin(PropFertile) ~ MeanTemp + MeanSummerTemp + AnnPrec + (1|species) + (1|blockID), fertileC, weights = CountOfspecies)
+summary(mod01)
+plot(mod01)
+
+
+
+
+# Does proportion flowering differ between PFG, early vs late flowering species and alpine/generalists?
+mod01 <- lmer(asin(PropFertile) ~ functionalGroup + (1|species) + (1|blockID), fertileC, weights = CountOfspecies)
+summary(mod01)
+
+
+
+
+
+
+
